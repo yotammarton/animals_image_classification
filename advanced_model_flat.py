f3ed64fd0ad32f1238ff87335ea8bbf961f34f94
Yotam Martin <yotamm@breezometer.com>
2020-08-28 10:36:22 +0300

    adv model example

---
+++ advanced_model_flat.py
@@ -0,0 +1,21 @@
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+
+model = ResNet50(weights=None)
+model.fit_generator()
+model.compile()
+
+img_path = 'images\miniature_pinscher_79.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+# decode the results into a list of tuples (class, description, probability)
+# (one such list for each sample in the batch)
+print('Predicted:', decode_predictions(preds, top=3)[0])
+
