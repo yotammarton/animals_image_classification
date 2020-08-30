61bd59ec2b8ce83574fb9ccd972f93bfb079dbd8
Yotam Martin <yotamm@breezometer.com>
2020-08-30 11:15:38 +0300

    Testing models... (simple CNN)

--- neural_structured_learning_model.py
+++ neural_structured_learning_model.py
@@ -10,10 +10,67 @@ import pandas as pd
 import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+"""###############################################"""
+"""###############################################"""
+"""###############################################"""
 IMAGE_INPUT_NAME = 'input_1'  # if experiencing problems with this change to value of 'model.layers[0].name'
 LABEL_INPUT_NAME = 'label'
 TRAIN_BATCH_SIZE = 32
-INPUT_SHAPE = [224, 224, 3]  # images will be resized to this shape, this is also the dims for layers
+INPUT_SHAPE = [128, 128, 3]  # images will be resized to this shape, this is also the dims for layers
+
+
+class HParams(object):
+    def __init__(self):
+        self.input_shape = INPUT_SHAPE
+        self.num_classes = 37
+        self.conv_filters = [32, 64, 64]
+        self.kernel_size = (3, 3)
+        self.pool_size = (2, 2)
+        self.num_fc_units = [64]
+        self.batch_size = 32
+        self.epochs = 30
+        self.adv_multiplier = 0.2
+        self.adv_step_size = 0.2
+        self.adv_grad_norm = 'infinity'
+
+
+HPARAMS = HParams()
+
+
+def build_base_model(hparams):
+    """Builds a model according to the architecture defined in `hparams`."""
+    inputs = tf.keras.Input(
+        shape=hparams.input_shape, dtype=tf.float32, name=IMAGE_INPUT_NAME)
+
+    x = inputs
+    for i, num_filters in enumerate(hparams.conv_filters):
+        x = tf.keras.layers.Conv2D(
+            num_filters, hparams.kernel_size, activation='relu')(
+            x)
+        if i < len(hparams.conv_filters) - 1:
+            # max pooling between convolutional layers
+            x = tf.keras.layers.MaxPooling2D(hparams.pool_size)(x)
+    x = tf.keras.layers.Flatten()(x)
+    for num_units in hparams.num_fc_units:
+        x = tf.keras.layers.Dense(num_units, activation='relu')(x)
+    pred = tf.keras.layers.Dense(hparams.num_classes, activation='softmax')(x)
+    model = tf.keras.Model(inputs=inputs, outputs=pred)
+    return model
+
+
+base_model = build_base_model(HPARAMS)
+
+base_model.compile(optimizer='adam', loss='categorical_crossentropy',
+                   metrics=['acc'])
+
+"""###############################################"""
+"""###############################################"""
+"""###############################################"""
+
+IMAGE_INPUT_NAME = 'input_1'  # if experiencing problems with this change to value of 'model.layers[0].name'
+LABEL_INPUT_NAME = 'label'
+TRAIN_BATCH_SIZE = 32
+INPUT_SHAPE = [128, 128, 3]  # images will be resized to this shape, this is also the dims for layers
 
 
 # TODO choose different shape?
@@ -62,17 +119,27 @@ test_dataset = tf.data.Dataset.from_generator(
 # for test data we dont want to generate infinite data, we just want the amount of data in the test (that's why take())
 test_dataset = test_dataset.take(len(test_df))  # Note: test_generator must have shuffle=False
 
+"""try simple model"""  # TODO delete this
+print('================== fit simple model ==================')
+print(base_model.summary())
+base_model.fit(train_generator, epochs=30, steps_per_epoch=np.ceil(len(train_df) / TRAIN_BATCH_SIZE))
+print('================== inference ==================')
+result = base_model.evaluate(test_dataset)
+print(dict(zip(base_model.metrics_names, result)))
+exit()
+
 """DEFINE BASE MODEL"""
 model = ResNet50(weights=None, classes=num_of_classes, input_shape=INPUT_SHAPE)
 # TODO  - basically in the tutorial they didn't fit this model. the only fit() call is for the adversarial.
 
-# test the ResNet50 model first
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
-model.fit(train_generator, epochs=30, steps_per_epoch=np.ceil(len(train_df) / TRAIN_BATCH_SIZE))
-model.save_weights('ResNet50_yotam_weights.h5')
-result = model.evaluate(test_dataset)
-print(dict(zip(model.metrics_names, result)))
-exit()
+# # test the ResNet50 model - not needed for NSL
+# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+# print('============ fit base model ============')
+# model.fit(train_generator, epochs=30, steps_per_epoch=np.ceil(len(train_df) / TRAIN_BATCH_SIZE))
+# model.save_weights('ResNet50_yotam_weights.h5')
+# print('================== inference ==================')
+# result = model.evaluate(test_dataset)
+# print(dict(zip(model.metrics_names, result)))
 
 """NSL"""
 adversarial_config = nsl.configs.make_adv_reg_config(multiplier=0.2, adv_step_size=0.2, adv_grad_norm='infinity')
@@ -80,6 +147,7 @@ adversarial_model = nsl.keras.AdversarialRegularization(model, label_keys=[LABEL
                                                         adv_config=adversarial_config)
 
 adversarial_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# TODO different loss?
 print('============ fit adversarial model ============')
 # every epoch we go through all the train data images
 adversarial_model.fit(train_dataset, epochs=30, steps_per_epoch=np.ceil(len(train_df) / TRAIN_BATCH_SIZE))
@@ -115,22 +183,6 @@ DEAD CODE ISLAND
 # def convert_to_tuples(features):
 #     return features[IMAGE_INPUT_NAME], features[LABEL_INPUT_NAME]
 
-# class HParams(object):
-#     def __init__(self):
-#         self.input_shape = [128, 128, 3]
-#         self.num_classes = 37
-#         self.conv_filters = [32, 64, 64]
-#         self.kernel_size = (3, 3)
-#         self.pool_size = (2, 2)
-#         self.num_fc_units = [64]
-#         self.batch_size = 1
-#         self.epochs = 5
-#         self.adv_multiplier = 0.2
-#         self.adv_step_size = 0.2
-#         self.adv_grad_norm = 'infinity'
-#
-#
-# HPARAMS = HParams()
 
 # datasets = tfds.load('mnist')
 
@@ -144,25 +196,6 @@ DEAD CODE ISLAND
 # test_dataset = mnist_test_dataset.map(normalize).batch(BATCH_SIZE).map(convert_to_tuples)
 
 #
-# def build_base_model(hparams):
-#     """Builds a model according to the architecture defined in `hparams`."""
-#     inputs = tf.keras.Input(
-#         shape=hparams.input_shape, dtype=tf.float32, name=IMAGE_INPUT_NAME)
-#
-#     x = inputs
-#     for i, num_filters in enumerate(hparams.conv_filters):
-#         x = tf.keras.layers.Conv2D(
-#             num_filters, hparams.kernel_size, activation='relu')(
-#             x)
-#         if i < len(hparams.conv_filters) - 1:
-#             # max pooling between convolutional layers
-#             x = tf.keras.layers.MaxPooling2D(hparams.pool_size)(x)
-#     x = tf.keras.layers.Flatten()(x)
-#     for num_units in hparams.num_fc_units:
-#         x = tf.keras.layers.Dense(num_units, activation='relu')(x)
-#     pred = tf.keras.layers.Dense(hparams.num_classes, activation='softmax')(x)
-#     model = tf.keras.Model(inputs=inputs, outputs=pred)
-#     return model
 
 
 #
