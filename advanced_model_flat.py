ddfa1c88b06e24c890f630af1b797de436d33899
Yotam Martin <yotamm@breezometer.com>
2020-09-08 18:08:51 +0300

    arrange flat model - 3 models

--- advanced_model_flat.py
+++ advanced_model_flat.py
@@ -1,7 +1,15 @@
+"""
+train and evaluate flat breed classification model for predicting animal breed (1 our of 37 classes)
+
+### Usage:
+python advanced_model_flat.py inception_v3
+or: python advanced_model_flat.py inception_resnet_v2
+or: python advanced_model_flat.py xception
+"""
+
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2
 from tensorflow.keras.applications.inception_resnet_v2 import preprocess_input as preprocess_input_inception_resnet_v2
-from tensorflow.keras.applications.densenet import DenseNet169
 from tensorflow.keras.applications.xception import Xception
 from tensorflow.keras.applications.xception import preprocess_input as preprocess_input_xception
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -12,7 +20,7 @@ import pandas as pd
 import sys
 
 model_name = sys.argv[1] if len(sys.argv) > 1 else ""
-TRAIN_BATCH_SIZE = 32 if model_name != 'xception' else 16
+TRAIN_BATCH_SIZE = 16 if model_name == 'xception' else 32
 
 print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
 print('NEW RUN FOR FLAT MODEL')
@@ -21,8 +29,8 @@ print('\n\n\nshear_range=0.2, zoom_range=0.2, horizontal_flip=True,'
       '\nwidth_shift_range=0.2, height_shift_range=0.2,'
       '\nrotation_range=20, brightness_range=[0.7, 1.1],')
 
-INPUT_SHAPE = [299, 299, 3]
 # images will be resized to this shape, this is also the dims for layers
+INPUT_SHAPE = [299, 299, 3]
 
 """LOAD DATAFRAMES"""
 
@@ -36,6 +44,7 @@ test_df = df[df['train/test'] == 'test'][['path', 'cat/dog', 'breed']]
 num_of_classes = len(set(train_df['breed']))
 
 """CREATE IMAGE GENERATORS"""
+"""TRAIN"""
 if model_name == 'inception_resnet_v2':
     pre_process = preprocess_input_inception_resnet_v2
     train_dataGen = ImageDataGenerator(shear_range=0.2, zoom_range=0.2, horizontal_flip=True,
@@ -48,7 +57,7 @@ elif model_name == 'xception':
                                        width_shift_range=0.2, height_shift_range=0.2,
                                        rotation_range=20, brightness_range=[0.7, 1.1],
                                        preprocessing_function=pre_process)
-else:
+else:  # inception_v3
     train_dataGen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True,
                                        width_shift_range=0.2, height_shift_range=0.2,
                                        rotation_range=20, brightness_range=[0.7, 1.1])
@@ -57,6 +66,7 @@ train_generator = train_dataGen.flow_from_dataframe(dataframe=train_df, x_col="p
                                                     class_mode="categorical", target_size=INPUT_SHAPE[:2],
                                                     batch_size=TRAIN_BATCH_SIZE)
 
+"""VALIDATION"""
 if model_name == 'inception_resnet_v2':
     pre_process = preprocess_input_inception_resnet_v2
     val_data_gen = ImageDataGenerator(preprocessing_function=pre_process)
@@ -70,6 +80,7 @@ val_generator = val_data_gen.flow_from_dataframe(dataframe=val_df, x_col="path",
                                                  class_mode="categorical", target_size=INPUT_SHAPE[:2],
                                                  batch_size=1, shuffle=False)
 
+"""TEST"""
 if model_name == 'inception_resnet_v2':
     pre_process = preprocess_input_inception_resnet_v2
     test_data_gen = ImageDataGenerator(preprocessing_function=pre_process)
@@ -83,37 +94,39 @@ test_generator = test_data_gen.flow_from_dataframe(dataframe=test_df, x_col="pat
                                                    class_mode="categorical", target_size=INPUT_SHAPE[:2],
                                                    batch_size=1, shuffle=False)  # batch_size=1, shuffle=False for test!
 
-"""PREPARE TENSORFLOW DATASETS FOR TEST"""
+"""PREPARE TENSORFLOW DATASETS FOR VALIDATION AND TEST"""
+val_dataset = tf.data.Dataset.from_generator(
+    lambda: val_generator,
+    output_types=(tf.float32, tf.float32))
+val_dataset = val_dataset.take(len(val_df))
+
 test_dataset = tf.data.Dataset.from_generator(
     lambda: test_generator,
     output_types=(tf.float32, tf.float32))
 # for test data we dont want to generate infinite data, we just want the amount of data in the test (that's why take())
 test_dataset = test_dataset.take(len(test_df))  # Note: test_generator must have shuffle=False
 
-val_dataset = tf.data.Dataset.from_generator(
-    lambda: val_generator,
-    output_types=(tf.float32, tf.float32))
-val_dataset = val_dataset.take(len(val_df))
-
+"""DEFINE MODEL"""
 if model_name == 'inception_v3':
     model = InceptionV3(weights=None, classes=num_of_classes)
 elif model_name == 'inception_resnet_v2':
     model = InceptionResNetV2(weights=None, classes=num_of_classes)
 elif model_name == 'xception':
     model = Xception(weights=None, classes=num_of_classes)
-elif model_name == 'densenet169':
-    model = DenseNet169(weights=None, classes=num_of_classes)
 else:
     raise ValueError(f"not supported model name {model_name}")
 
+"""MODEL PARAMS AND CALLBACKS"""
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 checkpoint = ModelCheckpoint(filepath=f'flat_weights_{model_name}.hdf5', save_best_only=True, verbose=1)
 early_stopping = EarlyStopping(monitor='val_accuracy', patience=10, verbose=1)
-reduce_lr = ReduceLROnPlateau(patience=5, verbose=1)  # TODO reduce / increase patience
+reduce_lr = ReduceLROnPlateau(patience=5, verbose=1)
 
+"""FIT MODEL"""
 print('============ fit flat model ============')
 model.fit(train_generator, epochs=100, steps_per_epoch=np.ceil(len(train_df) / TRAIN_BATCH_SIZE),
           validation_data=val_dataset, callbacks=[checkpoint, early_stopping, reduce_lr])
+# load the best (on validation) weights from .fit() phase
 model.load_weights(filepath=f'flat_weights_{model_name}.hdf5')
 
 print('============ predict flat model ============')
